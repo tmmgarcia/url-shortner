6b8c1d98c37a58af9e4fb268cb8bc62b0eb7cd18
Tiago Garcia <tiago.tiago22@gmail.com>
2019-07-18 02:00:05 -0300

    Add TODO to configure structure instead of schema

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module UrlShortner
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # TODO configure project to create structure instead of schema
+    # config.active_record.schema_format = :sql
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
