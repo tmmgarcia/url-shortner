3a30d3d880fe7142971d030b5ae3a4a6efa6386f
Tiago Garcia <tiago.tiago22@gmail.com>
2019-07-19 01:51:51 -0300

    Add validations to ShortUri model

--- app/models/short_uri.rb
+++ app/models/short_uri.rb
@@ -1,2 +1,4 @@
 class ShortUri < ApplicationRecord
+  validates :original_uri, :short_uri, presence: true
+  validates :original_uri, url: true
 end
---
+++ spec/models/short_uri_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe ShortUri, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:original_uri) }
+    it { is_expected.to validate_presence_of(:short_uri) }
+
+    context 'with valid parameters' do
+      it { expect(build(:short_uri).valid?).to be_truthy }
+    end
+
+    context 'with invalid parameters' do
+      it { expect(ShortUri.new.valid?).to be_falsey }
+      it { expect(build(:short_uri, original_uri: 'invalid_url').valid?).to be_falsey }
+    end
+  end
+
+  describe 'database columns' do
+    it { is_expected.to have_db_column(:original_uri).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:short_uri).of_type(:string).with_options(null: false)  }
+    it { is_expected.to have_db_column(:times_clicked).of_type(:integer).with_options(default: 0)  }
+  end
+end
